2268566fbfeb673821815eb0e53e3d4af2dcc08d
REVATHISARAISWATHI <REVATHISARAISWATHI@gmail.com>
2019-10-06 10:56:18 -0800

    my commit

---
+++ src/main/java/com/mavendevop/Add.java
@@ -0,0 +1,8 @@
+package com.mavendevop;
+
+public class Add {
+	public  int addf(int a,int b)
+	 {
+		return a+b;
+			}
+}
---
+++ src/test/java/com/mavendevop/Addtest.java
@@ -0,0 +1,15 @@
+package com.mavendevop;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+public class Addtest {
+
+	@Test
+	public void test() {
+		Add a=new Add();
+		int rs=a.addf(2,4);
+		assertEquals(6,rs);
+
+	}
+}
